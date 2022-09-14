4bad01bdb437f2bbf18a9b7dc6cf5c6aa4a8235e
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-09-14 16:23:19 +0300

    lab3

--- application/lib/header.php
+++ application/lib/header.php
@@ -3,13 +3,13 @@
        if (isset($_SESSION['isUser'])) {
         $arr = '
         <div class="title-block">
-        <h1 class = "title" > Majesty Developer: ' . $_SESSION['userFullname']. '</h1>
+        <a href="/">    <h1 class = "title" > About Reality developer: ' . $_SESSION['userFullname']. '</h1></a>
         </div>
         ';
        } else {
            $arr = '
            <div class="title-block">
-           <h1 class = "title" > Majesty Developer: (not authorized) </h1>
+           <a href="/">  <h1 class = "title" > About Reality developer: (not authorized) </h1></a>
            </div>
            ';
       
