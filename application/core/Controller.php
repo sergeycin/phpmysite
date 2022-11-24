976eeb9e45ce61abfecd21ff6d90c551ab8c2142
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-11-24 20:50:57 +0300

    review

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -12,10 +12,8 @@ abstract class Controller {
         $this->route = $route;
         $this->view = new View($route);
         $model = $route['controller'].'Model';
-       
+      
         $this->model = $this->loadModel($model);
-
-
     }
 
     public function loadModel($name) {
