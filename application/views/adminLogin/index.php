aeb6fc874976c6d3e8fac091db1a18c951c837df
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-12-09 12:02:10 +0300

    rename

--- application/views/adminLogin/index.php
+++ application/views/adminLogin/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Majesty Developer</title>
+    <title>Reality Developer</title>
     <link rel="stylesheet" href="/public/css/bootstrap.min.css">
     <link rel="stylesheet" href="/public/css/style.css">
 </head>
@@ -13,7 +13,7 @@
 <body>
     <header>
         <div class="title-block">
-            <h1 class="title">Majesty Developer</h1>
+            <h1 class="title">Reality Developer</h1>
         </div>
         <div class="time"></div>
     </header>
--- application/views/uploadPosts/index.php
+++ application/views/uploadPosts/index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title> Majesty Developer: (not autorized) </title>
+  <title> Reality Developer: (not autorized) </title>
 
 
   <link rel="stylesheet" href="/public/css/bootstrap.min.css">
